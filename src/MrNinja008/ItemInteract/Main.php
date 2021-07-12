53a30bbab01342fdcb444206c5a6a63be343825a
MrNinja008 <79526396+MrNinja008@users.noreply.github.com>
2021-07-12 12:22:48 +0530

    Update Main.php
    Formating, some Fixes

--- src/MrNinja008/ItemInteract/Main.php
+++ src/MrNinja008/ItemInteract/Main.php
@@ -5,95 +5,71 @@ declare(strict_types=1);
 namespace MrNinja008\ItemInteract;
 
 use pocketmine\plugin\PluginBase;
-
 use pocketmine\Player;
-
 use pocketmine\Server;
-
 use pocketmine\item\Item;
 
- 
-
 use pocketmine\command\CommandSender;
-
 use pocketmine\command\CommandExecutor;
-
 use pocketmine\utils\Config;
-
 use pocketmine\event\Listener;
-
 use pocketmine\event\player\PlayerJoinEvent;
-
 use pocketmine\event\player\PlayerInteractEvent;
 
 use pocketmine\event\inventory\InventoryTransactionEvent;
-
 use pocketmine\event\player\PlayerDropItemEvent;
 
 class Main extends PluginBase implements Listener{
 
     public function onEnable(){
-
+        
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
-
-       @mkdir($this->getDataFolder());
-
+        @mkdir($this->getDataFolder());
         $this->saveResource("config.yml");
-
         $this->cfg = new Config($this->getDataFolder() . "config.yml", Config::YAML);
 
     }
 
     public function onJoin(PlayerJoinEvent $event){
-
+        
         $player = $event->getPlayer();
-
         $item->getId($this->cfg->get("ItemID"));
-
         $item->setCustomName($this->cfg->get("DisplayName"));
-
         $player->getInventory()->setItem($this->cfg->get("HotBarSlot"), $item, true);
 
     }
 
-    
-
     public function onDrop(PlayerDropItemEvent $event) {
 
         $player = $event->getPlayer();
-
         $item = $event->getItem();
-
         $item->getId($this->cfg->get("ItemID"));
-
-        if($item->getId() == $this->getConfig()->getAll()[$args[0]]["ItemID"]){ 
-
-             $event->setCancelled(true);
+      if($item->getId() == $this->getConfig()->getAll()[$args[0]]["ItemID"]){ 
+        $event->setCancelled(true);
 
         }
 
     }     
 
-         
-
-     public function onClick(PlayerInteractEvent $event){
+    public function onClick(PlayerInteractEvent $event){
 
         $player = $event->getPlayer();
-
-        $itn = $player->getInventory()->getItemInHand()->getCustomName();
-
-         if($itn == $this->cfg->get("DisplayName")){
-
-            $this->getServer()->getCommandMap()->dispatch($player, $this->cfg->get("command"));
+        $item = $player->getInventory()->getItemInHand()->getCustomName();
+     if($item == $this->cfg->get("DisplayName")){
+        $this->getServer()->getCommandMap()->dispatch($player, $this->cfg->get("command"));
 
         } 
 
     }
-
-    public function onInventory(InventoryTransactionEvent $event){
-
-        $event->setCancelled(true);
-
+    
+    public function onTransaction(InventoryTransactionEvent $event){
+        $transaction = $event->getTransaction();
+        foreach($transaction->getActions() as $action){
+            $item = $action->getSourceItem();
+            $source = $transaction->getSource();
+            if($source instanceof Player && $item->getId($this->cfg->get("ItemID") && $item->hasCustomName($this->cfg->get("DisplayName")){
+                $event->setCancelled();
+            }
+        }
     }
-
 }
