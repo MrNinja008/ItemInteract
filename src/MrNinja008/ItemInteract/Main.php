7e96f7960dfb851fba87e5c1e5293c393d63d418
MrNinja008 <79526396+MrNinja008@users.noreply.github.com>
2021-07-15 12:23:59 +0530

    Update Main.php
    Lore Feature and fixes

--- src/MrNinja008/ItemInteract/Main.php
+++ src/MrNinja008/ItemInteract/Main.php
@@ -11,6 +11,7 @@ use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\event\player\PlayerInteractEvent;
 use pocketmine\event\inventory\InventoryTransactionEvent;
 use pocketmine\event\player\PlayerDropItemEvent;
+use function explode;
 
 class Main extends PluginBase implements Listener {
     public function onEnable() {
@@ -20,8 +21,15 @@ class Main extends PluginBase implements Listener {
 
     public function onJoin(PlayerJoinEvent $event) {
         $player = $event->getPlayer();
-        $item = Item::get($this->getConfig()->get("ItemID"));
+        $itemArray = explode(':', $this->getConfig()->get("ItemID"));
+        if(!isset($itemArray[0]) || !isset($itemArray[1]) || !isset($itemArray[2])) {
+                    $this->getLogger()->error("Config Error! Make Sure To Use ID:META:COUNT.");
+        continue;
+                }
+        $player = $event->getPlayer();
+        $item = Item::get((int)$itemArray[0], (int)$itemArray[1], (int)$itemArray[2]); //FORMAT ID:META:COUNT
         $item->setCustomName($this->getConfig()->get("DisplayName"));
+        $item->setLore($this->getConfig()->get("Lore")); //LORE BOI
         $item->getNamedTag()->setInt("ItemInteractPlugin", 1);
         $player->getInventory()->setItem($this->getConfig()->get("HotBarSlot"), $item, true);
     }
